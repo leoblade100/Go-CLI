169785515332e5c7f81089a9f5d689900d308638
Teekatus.R <teekatus02064@gmail.com>
2024-09-11 21:06:34 +0700

    flag

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	todos := Todos{}
 	todos.add("buy milk")
-	todos.add("buy milk")
-	todos.saveToFile("todo")
+	todos.add("buy products")
+	fmt.Println("%+v\n\n", todos)
+	todos.delete(2)
+	todos.togle(1)
+	fmt.Println("%+v\n\n", todos)
+	todos.togle(1)
+	fmt.Println("%+v\n\n", todos)
+	todos.edit(0, "buy game")
+	fmt.Println("%+v\n\n", todos)
+	todos.print()
 }
